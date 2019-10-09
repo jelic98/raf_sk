2dddc482380303700d37baa1e87cb6ee7d184362
Lazar Jelic <lazarjelic98@gmail.com>
2019-10-10 00:25:34 +0200

    Added project base

---
+++ storage-spec/src/main/java/rs/raf/storage/spec/core/Res.java
@@ -0,0 +1,10 @@
+package rs.raf.storage.spec.core;
+
+public class Res {
+
+    public static final String FORMAT_DATE = "dd. MMM YYYY";
+    public static final String FORMAT_TIME = "HH:mm:ss";
+    public static final String FORMAT_DATETIME = "dd. MMM YYYY, HH:mm:ss";
+
+    public static final String ERROR_SAMPLE = "This is a sample error message";
+}
