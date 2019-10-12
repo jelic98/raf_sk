7d808559d2710dd9e383f063015ec9b80f896a19
Lazar Jelic <lazarjelic98@gmail.com>
2019-10-12 23:21:37 +0200

    Using Konzole library via JitPack for demo app

--- storage-app/src/main/java/rs/raf/storage/app/App.java
+++ storage-app/src/main/java/rs/raf/storage/app/App.java
@@ -1,11 +1,36 @@
 package rs.raf.storage.app;
 
-import rs.raf.storage.app.model.Lifecycle;
-import rs.raf.storage.app.storage.StorageStructure;
+import com.konzole.framework.*;
 
 public class App {
 
     public static void main(String[] args) {
         new Lifecycle(new StorageStructure()).run();
+    }
+
+    private static final class StorageStructure extends Structure {
+
+        @Override
+        protected Question create() {
+            return new Question("What operation to execute?")
+                    .addOption(new Option("Upload file").setQuestion(new Question("Where to upload file?")
+                        .addOption(new ExecuteOption("Local storage") {
+                            @Override
+                            public void execute() {
+                                Input source = getInput("source");
+                                Input destination = getInput("destination");
+                                System.out.println(String.format("Uploading file from %s to %s", source.getValue(), destination.getValue()));
+                            }
+                        }
+                        .addInput(new Input("Source"))
+                        .addInput(new Input("Destination")))
+                    ))
+                    .addOption(new ExecuteOption("Download file") {
+                        @Override
+                        public void execute() {
+                            System.out.println("Downloading file...");
+                        }
+                    });
+        }
     }
 }
--- storage-app/src/main/java/rs/raf/storage/app/model/SelectionListener.java
+++
@@ -1,6 +0,0 @@
-package rs.raf.storage.app.model;
-
-interface SelectionListener {
-
-    void onSelected();
-}
